4d9e7259b092929a4c01049befd28a49f9ba1f60
abe-101 <abe@habet.dev>
2022-02-16 12:04:22 -0500

    Move-Zeros-To-End: solution in python

---
+++ Array/Move-Zeros-To-End/move_zeros_to_end.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def moveZeroes(self, nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    cnt = nums.count(0)
+    
+    while 0 in nums:
+        nums.remove(0)
+        
+    for i in range(cnt):
+        nums.append(0)
+
