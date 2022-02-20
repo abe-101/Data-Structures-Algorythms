14ad055fe48be2118eba4260ff6db7fb74d8811d
Abe <abe@habet.dev>
2022-02-20 11:35:52 -0500

    Solution in java

---
+++ Array/Subarray-with-Max-Sum/subarrayWithMaxSum.java
@@ -0,0 +1,12 @@
+public static Integer subarrayWithMaxSum(int[] a) {
+    if (a == null || a.length == 0)
+        throw new IllegalArgumentException("Input array is empty or null");
+
+    int result = a[0], maxEndingHere= a[0];
+
+    for (int i = 1; i < a.length; i++) {
+        maxEndingHere = Math.max(maxEndingHere + a[i], a[i]);
+        result = Math.max(result, maxEndingHere)
+    }
+    return result
+}
