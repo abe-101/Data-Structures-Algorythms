74e0e005ba9d27cd3f48dba87115b55d1a3fb0fa
Abe <abe@habet.dev>
2022-03-13 17:14:32 -0400

    add LinkedList Demo

---
+++ linked_list/Java-Implemantation/LinkedListDemo.java
@@ -0,0 +1,21 @@
+import LinkedList.*;
+
+
+class LinkedListDemo {
+    public static void main(String[] args) {
+        Node n = new Node(null, -1);
+        LinkedList l = new LinkedList(n, n);
+        for (int i = 0; i < 9; i++) {
+            n = new Node(null, i);
+            l.append(n);
+        }
+        Node node = new Node(null, -1);
+        for (int i = 1; i < 10; i++) {
+            node = l.get(i);
+            int data = node.getData();
+            System.out.println(data);
+        }
+
+    }
+}
+
