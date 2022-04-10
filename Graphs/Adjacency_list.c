59492b9995e6721fa141b6d4c53c7052e52c4023
Abe <abe@habet.dev>
2022-04-09 22:47:35 -0400

    add print graph

--- Graphs/Adjacency_list.c
+++ Graphs/Adjacency_list.c
@@ -3,6 +3,11 @@
 
 #define MAXV    100         // maximum number of vertices
 
+void initialize_graph(graph *g, boo directed);
+void read_graph(graph *g, bool directed);
+void insert_edge(graph * g, int x, int y, bool directed);
+void print_graph(graph *g);
+
 typedef struct edgenode {
     int y;                  // adjacency ino
     int weight;             // edge weight, if any
@@ -34,7 +39,7 @@ void initialize_graph(graph *g, boo directed) {
     }
 }
 
-Void read_graph(graph *g, bool directed) {
+void read_graph(graph *g, bool directed) {
     int i;              // counter
     int m;              // number of edges
     int x, y;           // vertices in edge (x,y)
@@ -58,7 +63,7 @@ void insert_edge(graph * g, int x, int y, bool directed) {
     p->y = y;
     p->next = g->edges[x];
 
-    g->edges[x] = p;        //insert at head of list
+    g->edges[x] = p;        // insert at head of list
     
     g->degree[x]++;
 
@@ -66,5 +71,20 @@ void insert_edge(graph * g, int x, int y, bool directed) {
         insert_edge(g, y, x, true);
     } else {
         g->nedges++;
+    }
+}
+
+void print_graph(graph *g) {
+    int i;          // counter
+    edgenode *p;    // temporary pointer
+
+    for (i = 1; i <= g->nvertices; i++) {
+        printf("%d: ", i);
+        p = g->edges[i];
+        while (p != NULL) {
+            printf(" %d", p->y);
+            p = p->next;
+        }
+        printf("\n");
     }
 }
