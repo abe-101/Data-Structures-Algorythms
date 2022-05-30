3613ff1ee18117d3d611461f5ffdb8808739e268
Abe <abe@habet.dev>
2022-05-30 12:10:39 -0400

    reverse list iterative

---
+++ python-solutions/linked-list/05-reverse_list.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Write a function, reverse_list, that takes in the head of a linked list
+as an argument. The function should reverse the order of the nodes in the 
+linked list in-place and return the new head of the reversed linked list.
+"""
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+# Iterative
+def reverse_list(head):
+    # Time: O(n)
+    # Space: O(1)
+    prev = None
+    current = head
+    while current is not None:
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    return prev
+
+# Recursive
+def reverse_list(head):
+    # Time:
+    # Space:
+
+# test_00:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+# a -> b -> c -> d -> e -> f
+assert(reverse_list(a) == f)
+# test_01:
+x = Node("x")
+y = Node("y")
+x.next = y
+# x -> y
+assert(reverse_list(x) == y) # y -> x
+# test_02:
+p = Node("p")
+# p
+assert(reverse_list(p) == p)# p
+print('-- Yay test completed!')
