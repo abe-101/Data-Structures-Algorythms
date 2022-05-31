939b44f605caa2eb8e8118352aee94fe09df6ef3
Abe <abe@habet.dev>
2022-05-31 17:53:43 -0400

    partital

---
+++ python-solutions/linked-list/11-insert_node.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+Write a function, insert_node, that takes in the head of a linked list, a
+value, and an index. The function should insert a new node with the value into
+the list at the specified index. Consider the head of the linked list as index
+0. The function should return the head of the resulting linked list.
+
+Do this in-place.
+
+You may assume that the input list is non-empty and the index is not greater
+than the length of the input list.
+"""
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+# Iterative
+def
+    # Time:
+    # Space:
+
+# Recursive
+def
+    # Time:
+    # Space:
+# test_00:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+a.next = b
+b.next = c
+c.next = d
+# a -> b -> c -> d
+assert(insert_node(a, 'x', 2)
+# a -> b -> x -> c -> d
+# test_01:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+a.next = b
+b.next = c
+c.next = d
+# a -> b -> c -> d
+assert(insert_node(a, 'v', 3)
+# a -> b -> c -> v -> d
+# test_02:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+a.next = b
+b.next = c
+c.next = d
+# a -> b -> c -> d
+assert(insert_node(a, 'm', 4)
+# a -> b -> c -> d -> m
+# test_03:
+a = Node("a")
+b = Node("b")
+a.next = b
+# a -> b
+assert(insert_node(a, 'z', 0)
+# z -> a -> b
+print('-- Yay # test completed!')
