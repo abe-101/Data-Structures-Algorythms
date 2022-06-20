dc342b5e3442a0b723ecec9c9fbeb911ef7f3b1b
Abe <abe@habet.dev>
2022-06-20 11:28:39 -0400

    bfs tree sum

--- Structy/binary-tree/03-tree_sum.py
+++ Structy/binary-tree/03-tree_sum.py
@@ -12,10 +12,31 @@ class Node:
         self.left = None
         self.right = None
 
+# def tree_sum(root):
+#     if root is None:
+#         return 0
+#     return root.val + tree_sum(root.left) + tree_sum(root.right)
+
+from collections import deque
+
 def tree_sum(root):
-    if root is None:
+    if not root:
         return 0
-    return root.val + tree_sum(root.left) + tree_sum(root.right)
+
+    queue = deque([ root ])
+    total_sum = 0
+
+    while queue:
+        node = queue.popleft()
+
+        total_sum += node.val
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    
+    return total_sum
 
 
 # test_00:
