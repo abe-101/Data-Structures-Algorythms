568fcd1f361e8a9c258d9752fb4498fd4773cf9f
Abe <abe@habet.dev>
2022-06-21 16:15:10 -0400

    add leaf lisr DF recursive

--- Structy/binary-tree/14-leaf_list.py
+++ Structy/binary-tree/14-leaf_list.py
@@ -34,9 +34,21 @@ def leaf_list(root):
     return leaves
 
 # Recursive
-#def
+def leaf_list(root):
     # Time:
     # Space:
+    leaves = []
+    _leaf_list(root, leaves)
+    return leaves
+
+def _leaf_list(root, leaves):
+    if root is None:
+        return
+    if root.left is None and root.right is None:
+        leaves.append(root.val)
+
+    _leaf_list(root.left, leaves)
+    _leaf_list(root.right, leaves)
 
 # test_00:
 
