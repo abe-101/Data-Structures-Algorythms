2623bc648aaa1be597750d5c65a27379b2b7c863
Abe <abe@habet.dev>
2022-06-21 14:33:51 -0400

    add tree levels DF recursive

--- Structy/binary-tree/12-tree_levels.py
+++ Structy/binary-tree/12-tree_levels.py
@@ -38,9 +38,26 @@ def tree_levels(root):
     return levels
 
 # Recursive
-#def tree_levels(root):
+def tree_levels(root):
     # Time:
     # Space:
+    levels = []
+    _tree_levels(root, levels, 0)
+    return levels
+
+def _tree_levels(root, levels, level_num):
+    if root is None:
+        return
+
+    if len(levels) == level_num:
+        levels.append([ root.val ])
+    else:
+        levels[level_num].append(root.val)
+
+    _tree_levels(root.left, levels, level_num + 1)
+    _tree_levels(root.right, levels, level_num + 1)
+
+    
 
 # test_00:
 
